41644b050263135dd5413ec844d12060f513330a
Arsène Rurangiza <a.rurangiza@gmail.com>
2023-04-22 19:09:51 +0200

    design draft: adds visit card design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
     './src/*.html'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica', 'Apple Color Emoji', 'Arial', 'sans-serif', 'Segoe UI Emoji', 'Segoe UI Symbol']
+    }
   },
-  plugins: [],
+  variants: {},
+  plugins: []
 }
 
